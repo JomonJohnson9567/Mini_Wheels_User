f3defe4a48d81795b1433eb7c0feb5cffb40fab1
Jomon Johnson <jomonjohsonkm@gmail.com>
2025-10-10 08:49:30 +0530

    first commit

--- lib/features/presentation/screens/filter_screen/widgets/price_slider.dart
+++ lib/features/presentation/screens/filter_screen/widgets/price_slider.dart
@@ -116,6 +116,7 @@ class PriceSlider extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
+                    
                     child: RangeSlider(
                       values: RangeValues(
                         sliderState.minPrice,
